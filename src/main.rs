60297baea4294389feb256f0a5a911efe75aa63d
XD2002 <xander.desoete@gmail.com>
2024-08-04 19:39:19 +0200

    add scrollarea to see all files

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 
 use eframe::egui::{self, CentralPanel};
 use eframe::{self};
+use egui::ScrollArea;
 use navigation::Navigator;
 use file_ops::{list_directory_contents, open_file};
 
@@ -37,20 +38,23 @@ impl eframe::App for FExpApp {
     // The `update` method is called every frame to update the UI
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let files = list_directory_contents(&self.navigator.current_path());
+
         CentralPanel::default().show(ctx, |ui| {
             ui.heading("Files");
-            for file in files {
-                if ui.button(file.clone()).clicked() {
-                    let full_path: PathBuf = self.navigator.current_path().join(file);
-                    if let Ok(metadata) = fs::metadata(full_path.clone()) {
-                        if metadata.is_file() {
-                            open_file(&full_path);
-                        } else if metadata.is_dir() {
-                            self.navigator.navigate_to(&full_path);
+            ScrollArea::vertical().show(ui, |ui| {
+                for file in files {
+                    if ui.button(file.clone()).clicked() {
+                        let full_path: PathBuf = self.navigator.current_path().join(file);
+                        if let Ok(metadata) = fs::metadata(full_path.clone()) {
+                            if metadata.is_file() {
+                                open_file(&full_path);
+                            } else if metadata.is_dir() {
+                                self.navigator.navigate_to(&full_path);
+                            }
                         }
                     }
                 }
-            }
+            });
         });
     }
 }
